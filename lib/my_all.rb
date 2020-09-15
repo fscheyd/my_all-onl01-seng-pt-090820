a48023053a6efd3e5566012b0694eb67807bcf3b
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-15 16:35:42 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,8 +3,9 @@ require 'pry'
 def my_all?(collection)
 binding.pry
   i  = 0
+  block_return_values = [ ]
   while i < collection.length 
-    yield(collection[i])
+    << yield(collection[i])
     i = i + 1
-
+  end
 end
